511fa11ebc7b342bde280c5630268988f82cb4d1
Andrea Murachelli <a.murachelli@nki.nl>
2016-09-22 16:10:11 +0200

    Revert "more files committed "
    This reverts commit 230f36111abe400ea4a349165af68c49cc60a17e.

--- EM_scripts/junk/blunt_cutters.py
+++
@@ -1,5 +0,0 @@
-from pprint import pprint
-from Bio.Restriction import AllEnzymes
-
-a = [e for e in AllEnzymes if e.is_blunt() and len(e) == 6 and ('N' in e.suppl) and ('M' in e.suppl) and e.opt_temp == 37]
-pprint([(name, name.site) for name in a]) 
